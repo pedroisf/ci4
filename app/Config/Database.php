905d504b51f0aa46f75fb7451ec3594e5e342998
Pedro Ferreira <pedroigorsferreira@gmail.com>
2025-01-28 16:33:41 -0300

    local .env and database default construct

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,33 +24,138 @@ class Database extends Config
      *
      * @var array<string, mixed>
      */
-    public $dsn = 'db_dsn_connect';
-    /**
-     * Sample database connection for Postgre.
-     *
-     * @var array<string, mixed>
-     */
     public array $default = [
         'DSN' => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'root',
-        'database' => 'ci4',
-        'schema' => 'public',
-        'DBDriver' => 'Postgre',
+        'password' => '',
+        'database' => '',
+        'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
         'DBDebug' => true,
-        'charset' => 'utf8',
+        'charset' => 'utf8mb4',
+        'DBCollat' => '',
         'swapPre' => '',
+        'encrypt' => false,
+        'compress' => false,
+        'strictOn' => false,
         'failover' => [],
-        'port' => 5432,
+        'port' => 3306,
+        'numberNative' => false,
+        'foundRows' => false,
         'dateFormat' => [
             'date' => 'Y-m-d',
             'datetime' => 'Y-m-d H:i:s',
             'time' => 'H:i:s',
         ],
     ];
+
+    //    /**
+    //     * Sample database connection for SQLite3.
+    //     *
+    //     * @var array<string, mixed>
+    //     */
+    //    public array $default = [
+    //        'database'    => 'database.db',
+    //        'DBDriver'    => 'SQLite3',
+    //        'DBPrefix'    => '',
+    //        'DBDebug'     => true,
+    //        'swapPre'     => '',
+    //        'failover'    => [],
+    //        'foreignKeys' => true,
+    //        'busyTimeout' => 1000,
+    //        'synchronous' => null,
+    //        'dateFormat'  => [
+    //            'date'     => 'Y-m-d',
+    //            'datetime' => 'Y-m-d H:i:s',
+    //            'time'     => 'H:i:s',
+    //        ],
+    //    ];
+
+    //    /**
+    //     * Sample database connection for Postgre.
+    //     *
+    //     * @var array<string, mixed>
+    //     */
+    //    public array $default = [
+    //        'DSN'        => '',
+    //        'hostname'   => 'localhost',
+    //        'username'   => 'root',
+    //        'password'   => 'root',
+    //        'database'   => 'ci4',
+    //        'schema'     => 'public',
+    //        'DBDriver'   => 'Postgre',
+    //        'DBPrefix'   => '',
+    //        'pConnect'   => false,
+    //        'DBDebug'    => true,
+    //        'charset'    => 'utf8',
+    //        'swapPre'    => '',
+    //        'failover'   => [],
+    //        'port'       => 5432,
+    //        'dateFormat' => [
+    //            'date'     => 'Y-m-d',
+    //            'datetime' => 'Y-m-d H:i:s',
+    //            'time'     => 'H:i:s',
+    //        ],
+    //    ];
+
+    //    /**
+    //     * Sample database connection for SQLSRV.
+    //     *
+    //     * @var array<string, mixed>
+    //     */
+    //    public array $default = [
+    //        'DSN'        => '',
+    //        'hostname'   => 'localhost',
+    //        'username'   => 'root',
+    //        'password'   => 'root',
+    //        'database'   => 'ci4',
+    //        'schema'     => 'dbo',
+    //        'DBDriver'   => 'SQLSRV',
+    //        'DBPrefix'   => '',
+    //        'pConnect'   => false,
+    //        'DBDebug'    => true,
+    //        'charset'    => 'utf8',
+    //        'swapPre'    => '',
+    //        'encrypt'    => false,
+    //        'failover'   => [],
+    //        'port'       => 1433,
+    //        'dateFormat' => [
+    //            'date'     => 'Y-m-d',
+    //            'datetime' => 'Y-m-d H:i:s',
+    //            'time'     => 'H:i:s',
+    //        ],
+    //    ];
+
+    //    /**
+    //     * Sample database connection for OCI8.
+    //     *
+    //     * You may need the following environment variables:
+    //     *   NLS_LANG                = 'AMERICAN_AMERICA.UTF8'
+    //     *   NLS_DATE_FORMAT         = 'YYYY-MM-DD HH24:MI:SS'
+    //     *   NLS_TIMESTAMP_FORMAT    = 'YYYY-MM-DD HH24:MI:SS'
+    //     *   NLS_TIMESTAMP_TZ_FORMAT = 'YYYY-MM-DD HH24:MI:SS'
+    //     *
+    //     * @var array<string, mixed>
+    //     */
+    //    public array $default = [
+    //        'DSN'        => 'localhost:1521/XEPDB1',
+    //        'username'   => 'root',
+    //        'password'   => 'root',
+    //        'DBDriver'   => 'OCI8',
+    //        'DBPrefix'   => '',
+    //        'pConnect'   => false,
+    //        'DBDebug'    => true,
+    //        'charset'    => 'AL32UTF8',
+    //        'swapPre'    => '',
+    //        'failover'   => [],
+    //        'dateFormat' => [
+    //            'date'     => 'Y-m-d',
+    //            'datetime' => 'Y-m-d H:i:s',
+    //            'time'     => 'H:i:s',
+    //        ],
+    //    ];
 
     /**
      * This database connection is used when running PHPUnit database tests.
@@ -93,6 +198,29 @@ class Database extends Config
         // we don't overwrite live data on accident.
         if (ENVIRONMENT === 'testing') {
             $this->defaultGroup = 'tests';
+        } else {
+            $this->default = [
+                'DSN' => '',
+                'hostname' => getenv('database.default.hostname'),
+                'username' => getenv('database.default.username'),
+                'password' => getenv('database.default.password'),
+                'database' => getenv('database.default.database'),
+                'schema' => 'public',
+                'DBDriver' => getenv('database.default.DBDriver'),
+                'DBPrefix' => getenv('database.default.DBPrefix'),
+                'pConnect' => false,
+                'DBDebug' => true,
+                'charset' => 'utf8',
+                'swapPre' => '',
+                'failover' => [],
+                'port' => getenv('database.default.port'),
+                'dateFormat' => [
+                    'date' => 'Y-m-d',
+                    'datetime' => 'Y-m-d H:i:s',
+                    'time' => 'H:i:s',
+                ],
+            ];
+    
         }
     }
 }
